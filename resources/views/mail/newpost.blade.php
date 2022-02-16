b77d00e71866919ac24fc3ef33a79da4da088af7
MishaBatsiashvili <mishabatsiashvili@yahoo.com>
2022-02-17 00:52:46 +0400

    test run for mail

--- resources/views/mail/newpost.blade.php
+++ resources/views/mail/newpost.blade.php
@@ -1,9 +1,29 @@
 <x-mail-layout>
+
+    <x-slot name="head">
+        <head>
+            <style>
+                @import url('https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500&family=Work+Sans:ital,wght@0,400;0,500;1,400;1,500&display=swap');
+
+                h1 {
+                    color: red;
+                    font-family: 'Roboto Slab', serif;
+                }
+
+                .text-yellow {
+                    color: yellow;
+                    font-family: 'Work Sans', sans-serif;
+                }
+
+            </style>
+        </head>
+    </x-slot>
+
     @if(isset($name) && isset($post))
     <div class="p-5">
-        <h1 class="font-bold text-lg">{{ $name }} just posted a new post</h1>
+        <h1>{{ $name }} just posted a new post</h1>
         <a
-            class="py-1.5 px-4 text-sm bg-cyan-500 hover:bg-cyan-600 rounded-lg text-white transition"
+            class="text-yellow"
             href={{ route('posts.show', ['post' => $post->id]) }}
         >
             Go to Post
